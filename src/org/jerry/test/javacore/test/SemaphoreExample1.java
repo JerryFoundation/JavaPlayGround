6afe3d49c9c002d1e163ef5eca7c2fac4203fc84
zhuqianchao <zhuqianchao@iboxpay.com>
2020-10-12 20:42:54 +0800

    add

--- src/org/jerry/test/javacore/test/SemaphoreExample1.java
+++ src/org/jerry/test/javacore/test/SemaphoreExample1.java
@@ -10,19 +10,19 @@ import java.util.concurrent.Semaphore;
  */
 public class SemaphoreExample1 {
     // 请求的数量
-    private static final int threadCount = 550;
+    private static final int threadCount = 20;
 
     public static void main(String[] args) {
         // 创建一个具有固定线程数量的线程池对象（如果这里线程池的线程数量给太少的话你会发现执行的很慢）
         ExecutorService threadPool = Executors.newCachedThreadPool();
         // 一次只能允许执行的线程数量。
-        final Semaphore semaphore = new Semaphore(20);
+        final Semaphore semaphore = new Semaphore(5);
 
         for (int i = 0; i < threadCount; i++) {
             final int threadnum = i;
             threadPool.execute(() -> {// Lambda 表达式的运用
                 try {
-                    semaphore.acquire();// 获取一个许可，所以可运行线程数量为20/1=20
+                    semaphore.acquire();// 获取一个许可，所以可运行线程数量为5/1=5
                     test(threadnum);
                     semaphore.release();// 释放一个许可
                 } catch (InterruptedException e) {
@@ -33,7 +33,6 @@ public class SemaphoreExample1 {
             });
         }
         threadPool.shutdown();
-        System.out.println("finish");
     }
 
     public static void test(int threadnum) throws InterruptedException {
