0da9b88a4aa16e68a37680c301f3f11b82e987af
zhuqianchao <zhuqianchao@iboxpay.com>
2020-10-13 22:27:31 +0800

    leetcode 24

---
+++ src/org/jerry/test/leetcode/LeetCode24.java
@@ -0,0 +1,76 @@
+package org.jerry.test.leetcode;
+
+import org.jerry.test.algorithm.ReverseList;
+
+import java.util.StringJoiner;
+
+/**
+ *
+ * 给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+ *
+ * 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+ *
+ *
+ * 输入：head = [1,2,3,4]
+ * 输出：[2,1,4,3]
+ * 示例 2：
+ *
+ * 输入：head = []
+ * 输出：[]
+ * 示例 3：
+ *
+ * 输入：head = [1]
+ * 输出：[1]
+ *
+ * @author zhuqianchao
+ * @date 13/10/2020 22:16
+ */
+public class LeetCode24 {
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+
+        @Override
+        public String toString() {
+            return new StringJoiner(", ", ListNode.class.getSimpleName() + "[", "]")
+                    .add("val=" + val)
+                    .add("next=" + next)
+                    .toString();
+        }
+    }
+
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode newHead = head.next;
+        head.next = swapPairs(newHead.next);
+        newHead.next = head;
+        return newHead;
+    }
+
+    public static void main(String[] args) {
+        ListNode n1 = new ListNode(1);
+        ListNode n2 = new ListNode(2);
+        n1.next = n2;
+        ListNode n3 = new ListNode(3);
+        n2.next = n3;
+        ListNode n4 = new ListNode(4);
+        n3.next = n4;
+
+        System.out.println(new LeetCode24().swapPairs(n1));
+    }
+}
